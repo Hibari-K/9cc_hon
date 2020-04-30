a5e9cbb32572ed60872ef79592803ce174eb40b7
Hibari-K <smiya_revaken@yahoo.co.jp>
2020-04-30 16:18:24 +0900

    impl +-*/codegen based on node

--- 9cc.c
+++ 9cc.c
@@ -112,7 +112,7 @@ Token *tokenize(char *p){
         /* debug */
         //printf("tokenize: %s\n", p);
 
-        if(*p == '+' || *p == '-'){
+        if(*p == '+' || *p == '-' || *p == '*' || *p == '/'){
             cur = new_token(TK_RESERVED, cur, p);
             p++;
             continue;
@@ -124,11 +124,113 @@ Token *tokenize(char *p){
             continue;
         }
 
-        error_at(token->str, "cannot tokenize");
+        //error_at(token->str, "cannot tokenize");
+        fprintf(stderr, "cannot tokenize '%s'\n", p);
+        exit(1);
     }
 
     new_token(TK_EOF, cur, p);
     return head.next;
+}
+
+
+/** Node **/
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+
+    return node;
+}
+
+// create new num node
+Node *new_node_num(int val){
+
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+
+    return node;
+}
+
+
+// primary = "(" expr ")" | num
+Node *primary(){
+
+    // if next token is "(", the expected node is '"(" expr ")"'
+    if(expectAndConsume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    // otherwise, it should be num
+    return new_node_num(expect_number());
+}
+
+// mul = primary ("*" primary | "/" primary)*
+Node *mul(){
+
+    Node *node = primary();
+
+    for(;;){
+        if(expectAndConsume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if(expectAndConsume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else 
+            return node;
+    }
+}
+
+// expr = mul ("+" mul | "-" mul)*
+Node *expr(){
+    
+    Node *node = mul();
+
+    for(;;){
+        if(expectAndConsume('+')){
+            node = new_node(ND_ADD, node, mul());
+        }
+        else if(expectAndConsume('-')){
+            node = new_node(ND_SUB, node, mul());
+        }
+        else
+            return node;
+    }
+}
+
+void codegen(Node *node){
+    if(node->kind == ND_NUM){
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    codegen(node->lhs);
+    codegen(node->rhs);
+
+    puts("    pop rdi");
+    puts("    pop rax");
+
+    switch(node->kind){
+
+        case ND_ADD:
+            puts("    add rax, rdi");
+            break;
+        case ND_SUB:
+            puts("    sub rax, rdi");
+            break;
+        case ND_MUL:
+            puts("    imul rax, rdi");
+            break;
+        case ND_DIV:
+            puts("    cqo");
+            puts("    idiv rdi");
+            break;
+    }
+
+    puts("    push rax");
 }
 
 
@@ -144,25 +246,16 @@ int main(int argc, char **argv){
     token = tokenize(argv[1]);
     //debug_token();
 
+    Node *node = expr();
+
     printf(".intel_syntax noprefix\n" \
            ".global main\n" \
            "main:\n");
     
-    // the first token is expected number
-    printf("    mov rax, %d\n", expect_number());
-    
-    while(!at_eof()){
+    codegen(node);
 
-        if(expectAndConsume('+')){
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-    }
-
-    printf("    ret\n");
+    puts("    pop rax");
+    puts("    ret");
 
     return 0;
 }
--- 9cc.h
+++ 9cc.h
@@ -15,3 +15,29 @@ struct Token{
     int val;
     char *str;
 };
+
+
+// kind of AST node
+typedef enum{
+    ND_ADD, 
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+// AST node type
+struct Node{
+    NodeKind kind;
+    Node *lhs; // left hand side
+    Node *rhs; // right hand side
+    int val;
+};
+
+
+// codegen
+Node* primary();
+Node* mul();
+Node* expr();
